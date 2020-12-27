fa59a3b7b298e71f1bc73d8c023becf047a34a5b
BURHANUDDIN <Bmerchant945@gmail.com>
2020-12-28 00:41:59 +0530

    separated the http request routes for files and workspaces the http request related routes have been moved to a separate folder these are now imprted in server.js

--- server.js
+++ server.js
@@ -4,65 +4,18 @@ let server = require("http").createServer(app);
 let io = require("socket.io")(server);
 const path = require("path");
 const fs = require("fs");
-const { execSync } = require("child_process");
+
 const public_dir = path.join(__dirname);
 app.use(express.static(public_dir));
-app.get("/public", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-app.get("/public/:filename", (req, res) => {
-  const workspace = req.query.workspace;
-  const file_path =
-    __dirname + "/workspaces/" + workspace + "/" + req.params.filename;
-  fs.open(file_path, "a+", (err, file) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("open");
-  });
-  fs.readFile(file_path, "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send({ error: err });
-    } else {
-      console.log(data);
-      res.send({ data });
-    }
-  });
-});
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/workspaces.html");
-});
-app.get("/workspaces/:workspace_name", (req, res) => {
-  const workspaceName = req.params.workspace_name;
+const PORT = process.env.PORT || 3000;
+//import the filerouter
+const fileRouter = require("./routers/filehandler");
+app.use(fileRouter);
 
-  fs.readdir(__dirname + "/workspaces/", (err, files) => {
-    let exists = false;
-    for (file in files) {
-      if (files[file] === workspaceName) {
-        console.log("here");
-        exists = true;
-        break;
-      }
-    }
-    if (exists) {
-      res.send({ data: "redirecting to previously existing directory" });
-    } else {
-      fs.mkdir(__dirname + "/workspaces/" + workspaceName, (err) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log("directory created");
-      });
-      res.send({ data: "creating direcory " + workspaceName });
-    }
-  });
-});
-const log = console.log;
 io.on("connection", (socket) => {
-  log("connected");
+  console.log("connected");
   socket.on("message", (evt) => {
-    log(evt);
+    console.log(evt);
     fs.writeFile(
       __dirname + "/workspaces" + evt.file,
       evt.text,
@@ -76,9 +29,9 @@ io.on("connection", (socket) => {
   });
 });
 io.on("disconnect", (evt) => {
-  log("some people left");
+  console.log("some people left");
 });
-const PORT = 3000;
+
 server.listen(PORT, () => {
   console.log("Server up on Port : ", PORT);
 });
