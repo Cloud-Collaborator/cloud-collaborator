b6ee1743eb175bcf919639c05f145d967ff8a36d
BURHANUDDIN <Bmerchant945@gmail.com>
2020-12-28 21:06:09 +0530

    load existing files directly from the dropdown

--- public/js/index.js
+++ public/js/index.js
@@ -28,12 +28,41 @@ if (!workspace) {
         .getElementById("container")
         .appendChild(label)
         .appendChild(fileMenu);
+      fileMenu.addEventListener("change", loadExistingFile);
     });
 }
 let cwd = "";
 const loadFile = () => {
   let fileNameValue = fileName.value;
   // let workspaceValue = workspaceName.value;
+  let workspaceValue = workspace;
+  if (!workspaceValue) {
+    location.href = BASE_URL;
+  } else {
+    if (fileNameValue) {
+      const url =
+        BASE_URL + "/public/" + fileNameValue + "?workspace=" + workspaceValue;
+      fetch(url)
+        .then((res) => res.json())
+        .then((resp) => {
+          if (resp.err) {
+            //debugger
+
+            console.log(resp.simply);
+          }
+          document.getElementById("editor").value = resp.data;
+          cwd = "/" + workspaceValue + "/" + fileNameValue;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      console.log("Filename Not input");
+    }
+  }
+};
+const loadExistingFile = () => {
+  const fileNameValue = document.getElementById("file-menu").value;
   let workspaceValue = workspace;
   if (!workspaceValue) {
     location.href = BASE_URL;
